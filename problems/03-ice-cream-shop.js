ab0fda164a5301f1ae656a9d74227765ec9c5763
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-11-07 18:05:56 +0000

    problem 3 complete

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -13,9 +13,24 @@ iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
 
 
-// your code here
+// set default value of i to 0 and then increment i on each recursive call if i is less than the array length
+// if not found then return false
 
+let iceCreamShop = (flavors, favorite, i=0) => {
+  if (favorite == flavors[i]){
+    return true;
+  } else if (i<flavors.length) {
+    return iceCreamShop(flavors, favorite, (i+1));
+  } else {
+    return false;
+  }
 
+}
+iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
+iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
+iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
+iceCreamShop(['moose tracks'], 'moose tracks'); // true
+iceCreamShop([], 'honey lavender'); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = iceCreamShop;
