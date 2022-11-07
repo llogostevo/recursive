6e0e30e96c30e3ea38cd75d64b3485b145b872cf
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-11-07 13:09:46 +0000

    problem 2 solved

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -10,8 +10,21 @@ sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
-// your code here
-  
+// set i as a default 0 to allow the first passing to occur. 
+let sumArray = (array, i=0)=> {
+  // check if hte array has reached the end
+  if (i==array.length){
+    // if it has then there should be no more values added so set the return to 0
+    return 0;
+  } else {
+    // add the current element to the element above
+    return array[i]+sumArray(array, (i+1))
+  }
+
+};
+sumArray([1, 2, 3]); //  6
+sumArray([0, 1, -3]); //  -2
+sumArray([1, 2, 3, 4, 5]); // 15
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
